3b08b712bfc1a5cce6fac3c5c44f3e7013a30ecb
Julien Richard <julien.richard@filigran.io>
2024-11-03 00:51:39 +0100

    [backend] Adapt cache user invalidation (#8877)

--- opencti-platform/opencti-graphql/src/database/engine.js
+++ opencti-platform/opencti-graphql/src/database/engine.js
@@ -101,6 +101,7 @@ import {
   executionContext,
   INTERNAL_USERS,
   isBypassUser,
+  isUserInsidePlatformOrganization,
   MEMBER_ACCESS_ALL,
   SYSTEM_USER,
   userFilterStoreElements
@@ -471,7 +472,7 @@ export const buildDataRestrictions = async (context, user, opts = {}) => {
     // If user have organization management role, he can bypass this restriction.
     // If platform is for specific organization, only user from this organization can access empty defined
     const settings = await getEntityFromCache(context, user, ENTITY_TYPE_SETTINGS);
-    // We want to exlucde a set of entities from organization restrictions while forcing restrictions for an other set of entities
+    // We want to exclude a set of entities from organization restrictions while forcing restrictions for another set of entities
     const excludedEntityMatches = {
       bool: {
         must: [
@@ -491,7 +492,7 @@ export const buildDataRestrictions = async (context, user, opts = {}) => {
       }
     };
     if (settings.platform_organization) {
-      if (user.inside_platform_organization) {
+      if (isUserInsidePlatformOrganization(settings, user)) {
         // Data are visible independently of the organizations
         // Nothing to restrict.
       } else {
