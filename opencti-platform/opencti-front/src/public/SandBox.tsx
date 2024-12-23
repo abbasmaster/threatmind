a2ca2d30d133795e4b31d4221df0985cae07a7ea
Angelique <angelique.jard@filigran.io>
2024-12-23 16:45:44 +0100

    sandbox frontend and graphql

--- opencti-platform/opencti-front/src/public/SandBox.tsx
+++ opencti-platform/opencti-front/src/public/SandBox.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ApolloSandbox } from '@apollo/sandbox/react';
 import makeStyles from '@mui/styles/makeStyles';
+import { EmbeddableSandboxOptions } from '@apollo/sandbox/src/EmbeddedSandbox';
 
 const useStyles = makeStyles({
   sandbox: {
@@ -11,10 +12,17 @@ const useStyles = makeStyles({
 
 const SandBox = () => {
   const classes = useStyles();
+  const sandboxOpts: Omit<EmbeddableSandboxOptions, 'target'> = {
+    initialEndpoint: 'http://localhost:4000/graphql',
+    initialState: {
+      includeCookies: true,
+    },
+  };
+
   return (
     <ApolloSandbox
-      initialEndpoint='http://localhost:4000'
       className={classes.sandbox}
+      {...sandboxOpts}
     />
   );
 };
